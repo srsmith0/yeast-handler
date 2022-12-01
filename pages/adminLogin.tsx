6f94a5d27334e8c90a149db87a4b678ff2c14a48
Shawn Smith <65311872+srsmith0@users.noreply.github.com>
2022-12-01 11:41:49 -0700

    added landing and views pages

---
+++ pages/adminLogin.tsx
@@ -0,0 +1,6 @@
+
+const adminLogin = () => {
+    return 'This is the admin login'
+};
+
+export default adminLogin;
---
+++ pages/viewLogin.tsx
@@ -0,0 +1,6 @@
+
+const viewLogin = () => {
+    return 'This is the view login page'
+};
+
+export default viewLogin;
